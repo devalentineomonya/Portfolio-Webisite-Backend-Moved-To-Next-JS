d49e2e1d6abe7c6d25aa5cd2219f9eae940fd244
Valentine Omonya <valomosh254@gmail.com>
2024-06-07 19:08:14 +0300

    Completed the folder strucure by adding some necessary files

---
+++ server.js
@@ -0,0 +1,56 @@
+/*========================
+CONSTANT IMPORTS
+==========================*/
+
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDb = require('./src/config/databaseConnection')
+const app = express()
+
+
+/*========================
+UNIVERSAL MIDDLEWARE
+========================== */
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+
+/*===========================
+UNIVERSAL VARIABLES 
+============================= */
+const port = process.env.PORT || 3000
+
+
+
+/*===========================
+API ROUTE ENDPOINTS
+=============================*/
+app.get("/",(req, res) => {
+    res.send("Welcome to devalentine API 😎")
+})
+
+
+
+
+
+/*==========================
+START NODE SERVER 
+============================ */
+const start = () =>{
+    try {
+        connectDb()
+        app.listen(port, ()=>{
+            console.log("Sever running on port " + port);
+        })
+
+    } catch (error) {
+        console.log("An error occured while starting node server", error.message);
+    }
+}
+
+start()
+
+
+
+
