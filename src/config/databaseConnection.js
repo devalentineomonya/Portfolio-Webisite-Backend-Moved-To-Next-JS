7e43313d2ea7bf84d57efba473940195841e604d
Valentine Omonya <valomosh254@gmail.com>
2024-06-07 18:55:49 +0300

    Initialized node project and created project structore

---
+++ src/config/databaseConnection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+const connectDb = async () => {
+    try {
+        const connectionString = process.env.CONNECTION_STRING || "mongodb://localhost:27017/devalentineProjects";
+        await mongoose.connect(connectionString).then(() => {
+            console.log("Database connected successfully.");
+        })
+    } catch (error) {
+        console.log("An error occurred while connecting to MongoDB", error.message);
+    }
+}
+
+module.exports = connectDb
