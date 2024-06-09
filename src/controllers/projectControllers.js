3779022f70101fa490d93c0893dbe27ad0a1cb61
Valentine Omonya <valomosh254@gmail.com>
2024-06-09 18:19:53 +0300

    Added all operations on projects route

--- src/controllers/projectControllers.js
+++ src/controllers/projectControllers.js
@@ -1,7 +1,16 @@
 const { unlink } = require('../middlewares/uploadMiddleware');
 const projectModel = require('../models/projectModels');
 
+const checkProjectDuplicate = async (name, githubLink, liveLink) => {
+    return await projectModel.findOne({
+        $or: [
+            { name },
+            { githubLink },
+            { liveLink }
 
+        ]
+    })
+}
 
 const addProject = async (req, res) => {
     const name = req.body.name
@@ -11,19 +20,9 @@ const addProject = async (req, res) => {
     const githubLink = req.body.githubLink
     const liveLink = req.body.liveLink
 
-
-
-
     try {
 
-        const checkProject = await projectModel.findOne({
-            $or: [
-                { name },
-                { githubLink },
-                { liveLink }
-
-            ]
-        })
+        const checkProject = await checkProjectDuplicate(name, githubLink, liveLink)
 
         if (!checkProject) {
             const newProject = new projectModel({
@@ -59,12 +58,60 @@ const listProjects = async (req, res) => {
 
 }
 
-const updateProject = (req, res) => {
+
+
+const updateProject = async (req, res) => {
+    const projectId = req.params.id
+    const name = req?.body?.name
+    const description = req?.body?.description
+    const technologies = req?.body?.technologies
+    const image = req?.file?.filename
+    const githubLink = req?.body?.githubLink
+    const liveLink = req?.body?.liveLink
+    try {
+        const project = await projectModel.findById(projectId)
+
+        if (project) {
+            const checkProject = await checkProjectDuplicate(name, githubLink, liveLink)
+            if (!checkProject) {
+                const updatedProject = await projectModel.findByIdAndUpdate(projectId, {
+                    name,
+                    description,
+                    technologies,
+                    image,
+                    githubLink,
+                    liveLink
+                },
+                    {
+                        new: true
+                    })
+                return res.status(200).json({ success: true, message: "Project updated successfully", data: updatedProject })
+            }
+            return res.status(400).json({ success: false, message: "Project with the same name, live link or github link already exists" })
+        }
+        res.status(404).json({ success: false, message: "Project wit the specifies id was not found" })
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: "An error occurred while updating project" + error.message })
+    }
+
 
 }
 
 
-const deleteProject = (req, res) => {
+const deleteProject = async (req, res) => {
+    const projectId = req.params.id
+    try {
+        const project = await projectModel.findById(projectId)
+        if (project) {
+            await projectModel.findByIdAndDelete(projectId)
+            unlink(project.image)
+            return res.status(200).json({ success: false, message: "Project deleted successfully" })
+        }
+        res.status(404).json({ success: false, message: "Project with specified was not found" })
+    } catch (error) {
+        res.status(500).json({ success: false, message: "An error occurred while deleting project " + error.message })
+    }
 
 }
 
--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -4,12 +4,12 @@ const projectRouter = express.Router();
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddlewares');
 const { upload } = require('../middlewares/uploadMiddleware');
 
-projectRouter.get("list", listProjects)
+projectRouter.get("/list", listProjects)
 
 projectRouter.post("/add", authMiddleware, isAdmin,upload.single("image"), addProject)
 
-projectRouter.put("/update", authMiddleware, isAdmin, updateProject)
+projectRouter.put("/update/:id", authMiddleware, isAdmin, updateProject)
 
-projectRouter.delete("/delete", authMiddleware, isAdmin, deleteProject)
+projectRouter.delete("/delete/:id", authMiddleware, isAdmin, deleteProject)
 
 module.exports = projectRouter
