5b590ea16300b0bb53fc09f3eddb02a0fe17c4e1
Valentine Omonya <valomosh254@gmail.com>
2024-06-18 21:16:24 +0300

    Added update languages route

--- src/routes/languageRoutes.js
+++ src/routes/languageRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { addLanguage, getLanguages } = require('../controllers/languageControllers')
+const { addLanguage, getLanguages, updateLanguage } = require('../controllers/languageControllers')
 const languagesRouter = express.Router()
 
  languagesRouter.post("/add", addLanguage)
  languagesRouter.get("/list", getLanguages)
+ languagesRouter.put("/update", updateLanguage)
 
 
  module.exports = languagesRouter
