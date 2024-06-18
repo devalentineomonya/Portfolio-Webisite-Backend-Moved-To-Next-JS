8eba8375152a893d4ae560732f0865306e0b98ae
Valentine Omonya <valomosh254@gmail.com>
2024-06-18 21:17:30 +0300

    Added language delete route

--- src/routes/languageRoutes.js
+++ src/routes/languageRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { addLanguage, getLanguages, updateLanguage } = require('../controllers/languageControllers')
+const { addLanguage, getLanguages, updateLanguage, deleteLanguage } = require('../controllers/languageControllers')
 const languagesRouter = express.Router()
 
  languagesRouter.post("/add", addLanguage)
  languagesRouter.get("/list", getLanguages)
  languagesRouter.put("/update", updateLanguage)
+ languagesRouter.delete("/delete", deleteLanguage)
 
 
  module.exports = languagesRouter
